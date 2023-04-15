8c52cf8a5b989d20f36dc56b2906f9503c827479
Brice Theurillat <brice.theurillat@protonmail.ch>
2023-04-15 19:50:29 +0200

    Adding 404 handling

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,6 @@ use std::{
     net::{TcpListener, TcpStream},
 };
 
-
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
 
@@ -15,18 +14,29 @@ fn main() {
     }
 }
 
+fn status_code_to_text(status_code: i32) -> Option<&'static str> {
+    match status_code {
+        200 => Some("OK"),
+        404 => Some("Not found"),
+        _ => None
+    }
+}
+
 fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&mut stream);
-    let _http_request: Vec<_> = buf_reader.lines()
-        .map(|result| result.unwrap())
-        .take_while(|line| !line.is_empty())
-        .collect();
+    let request_line = buf_reader.lines().next().unwrap().unwrap();
 
-    let status_line = "HTTP/1.1 200 OK";
-    let contents = fs::read_to_string("hello.html").unwrap();
+    let (html_file, status_code) = if request_line == "GET / HTTP/1.1" {
+        ("hello.html", 200)
+    } else {
+        ("404.html", 404)
+    };
+
+    let status_text = status_code_to_text(status_code).unwrap();
+    let status_line = format!("HTTP/1.1 {status_code} {status_text}");
+    let contents = fs::read_to_string(html_file).unwrap();
     let length = contents.len();
 
     let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
-
     stream.write_all(response.as_bytes()).unwrap();
 }
